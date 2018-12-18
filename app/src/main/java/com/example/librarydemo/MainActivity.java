f398ddd0335c462ed9d3cfc297146b9041a26c3a
umesh.saravane <ume7715@gmail.com>
2018-12-18 14:03:34 +0530

    Initial commit

--- app/src/main/java/com/example/librarydemo/MainActivity.java
+++ app/src/main/java/com/example/librarydemo/MainActivity.java
@@ -3,11 +3,13 @@ package com.example.librarydemo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.example.demolibrary.ToastLibrary;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
+        }
 }
--- demolibrary/src/main/java/com/example/demolibrary/ToastLibrary.java
+++ demolibrary/src/main/java/com/example/demolibrary/ToastLibrary.java
@@ -1,3 +1,5 @@
+package com.example.demolibrary;
+
 import android.content.Context;
 import android.widget.Toast;
 
